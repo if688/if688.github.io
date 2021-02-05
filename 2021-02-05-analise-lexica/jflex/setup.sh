d00b05882a4a785f772de689082320f9b20b7a73
Leopoldo Teixeira <lmt@cin.ufpe.br>
2021-02-05 09:57:36 -0300

    código base a ser usado na aula de hoje

---
+++ 2021-02-05-analise-lexica/jflex/setup.sh
@@ -0,0 +1,3 @@
+wget https://github.com/jflex-de/jflex/releases/download/v1.8.2/jflex-1.8.2.tar.gz
+tar -xf jflex-1.8.2.tar.gz
+rm jflex-1.8.2.tar.gz
---
+++ 2021-02-05-analise-lexica/jflex/src/main/java/br/ufpe/cin/compilers/jflex/App.java
@@ -0,0 +1,11 @@
+package br.ufpe.cin.compilers.jflex;
+
+public class App {
+    public String getGreeting() {
+        return "Hello world.";
+    }
+
+    public static void main(String[] args) {
+
+    }
+}
