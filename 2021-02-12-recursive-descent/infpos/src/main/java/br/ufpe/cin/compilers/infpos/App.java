8abfe5f7d6514bb7aeb30957e933993d5a7adee6
Leopoldo Teixeira <lmt@cin.ufpe.br>
2021-02-12 11:59:30 -0300

    código ao fim da aula

--- 2021-02-12-recursive-descent/infpos/src/main/java/br/ufpe/cin/compilers/infpos/App.java
+++ 2021-02-12-recursive-descent/infpos/src/main/java/br/ufpe/cin/compilers/infpos/App.java
@@ -1,11 +1,15 @@
 package br.ufpe.cin.compilers.infpos;
 
 import br.ufpe.cin.compilers.lexer.LexerManual;
+import br.ufpe.cin.compilers.parser.Parser;
 
 public class App {
-    public static String programa ="22+4-10";
+    public static String programa ="22 + 4 - 10";
 
     public static void main(String[] args) {
+
         LexerManual lexer = new LexerManual(programa);
+        Parser parser = new Parser(lexer);
+        parser.parse();
     }
 }
--- 2021-02-12-recursive-descent/infpos/src/main/java/br/ufpe/cin/compilers/parser/Parser.java
+++ 2021-02-12-recursive-descent/infpos/src/main/java/br/ufpe/cin/compilers/parser/Parser.java
@@ -1,8 +1,74 @@
 package br.ufpe.cin.compilers.parser;
 
 
+import br.ufpe.cin.compilers.lexer.ILexer;
+import br.ufpe.cin.compilers.lexer.TipoToken;
+import br.ufpe.cin.compilers.lexer.Token;
+
 public class Parser {
 
+    ILexer lexer;
+    private Token tokenAtual;
 
+    public Parser(ILexer l) {
+        lexer = l;
+        getProximoToken();
+    }
 
+    void getProximoToken() {
+        tokenAtual = lexer.getToken();
+    }
+
+    void erro(String msg) {
+        throw new ParsingException(msg);
+    }
+
+    void match(TipoToken esperado) {
+        if (tokenAtual.tipo == esperado) {
+            getProximoToken();
+        }
+        else {
+            erro("Esperava por token do tipo "+esperado.name()+", apareceu "+tokenAtual.tipo.name());
+        }
+    }
+
+    public void parse() {
+        expr();
+        System.out.println("Tudo ok com a entrada");
+    }
+
+    void expr() {
+        term(); rest();
+    }
+
+    void term() {
+        if (tokenAtual.tipo == TipoToken.NUM) {
+            System.out.print(tokenAtual.lexema + ' ');
+            match(TipoToken.NUM);
+        }
+        else {
+            erro("erro de sintaxe");
+        }
+    }
+
+    void rest() {
+        if (tokenAtual.tipo == TipoToken.ADICAO) {
+            match(TipoToken.ADICAO);
+            term();
+            System.out.print("+ ");
+            rest();
+        }
+        else if (tokenAtual.tipo == TipoToken.SUBTRACAO) {
+            match(TipoToken.SUBTRACAO);
+            term();
+            System.out.print("- ");
+            rest();
+        }
+        else if(tokenAtual.tipo == TipoToken.EOF) {
+            //palavra vazia
+        }
+        else {
+            erro("erro de sintaxe, token inesperado: "+tokenAtual.tipo.name());
+        }
+    }
 }
