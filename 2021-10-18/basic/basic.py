d1b74168cabf3395dcced74f55effc8780e9d974
Leopoldo Teixeira <lmt@cin.ufpe.br>
2021-10-18 09:55:55 -0300

    BASIC starter code for AST

---
+++ 2021-10-18/basic/basic.py
@@ -0,0 +1,17 @@
+from lexer import *
+from parser import *
+import sys
+
+def main(): 
+    if len(sys.argv) != 2:
+        sys.exit("Erro: Precisamos de um arquivo como argumento.")
+    with open(sys.argv[1], 'r') as inputFile:
+        input = inputFile.read()
+
+    lexer = Lexer(input)
+    parser = Parser(lexer)
+    parser.parse()
+    print("Terminamos.")
+
+
+main()
