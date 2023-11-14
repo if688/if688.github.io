1ed8488c4cc47c8c81477310eec769217f298c79
Leopoldo Teixeira <lmt@cin.ufpe.br>
2023-11-14 11:52:50 -0300

    material para início da aula de 14.11.2023

---
+++ 2023-11-14/basic/basic.py
@@ -0,0 +1,17 @@
+from lex import *
+from parse import *
+import sys
+
+def main(): 
+    if len(sys.argv) != 2:
+        sys.exit("Erro: Precisamos de um arquivo como argumento.")
+    with open(sys.argv[1], 'r') as inputFile:
+        input = inputFile.read()
+
+    lexer = Lexer(input)
+    parser = Parser(lexer)
+    parser.parse()
+    print("Terminamos.")
+
+
+main()
