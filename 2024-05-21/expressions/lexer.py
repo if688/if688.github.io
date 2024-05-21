ffdd4cf0d1c33554b09bbde509834bfe40f3c29c
Leopoldo Teixeira <lmt@cin.ufpe.br>
2024-05-21 10:08:47 -0300

    inicio da aula de 21.05.2024

---
+++ 2024-05-21/expressions/lexer.py
@@ -0,0 +1,92 @@
+import enum
+import sys
+
+class Lexer:
+    def __init__(self, source):
+        self.source = source + '\n'
+        self.caractereAtual = ''
+        self.posicaoAtual = -1
+        self.proximoCaractere()
+    
+    # Processa o próximo caractere
+    def proximoCaractere(self):
+        self.posicaoAtual+=1
+        if self.posicaoAtual >= len(self.source):
+            self.caractereAtual = None #EOF
+        else:
+            self.caractereAtual = self.source[self.posicaoAtual]
+
+    # Retorna o caractere de lookahead (qual o que vem aí)
+    def peek(self):
+        if self.posicaoAtual+1 >= len(self.source):
+            return None #EOF
+        else:
+            return self.source[self.posicaoAtual+1]
+
+    # Token inválido, imprime mensagem de erro
+    def abort(self, message):
+        sys.exit('Erro Léxico: '+message)
+		
+    # Pula espaços em branco
+    def skipWhitespace(self):
+        while self.caractereAtual == ' ' or self.caractereAtual == '\t' or self.caractereAtual == '\n': 
+            self.proximoCaractere()
+		
+    # Pula comentários
+    def skipComment(self):
+        pass
+
+    # Retorna o próximo token
+    def getToken(self):
+        self.skipWhitespace()
+        token = None
+        if self.caractereAtual is None: 
+            token = Token(self.caractereAtual, TipoToken.EOF)
+        elif self.caractereAtual == '+':
+            token = Token(self.caractereAtual, TipoToken.SOMA)
+        elif self.caractereAtual == '-':
+            token = Token(self.caractereAtual, TipoToken.SUBTRACAO)
+        elif self.caractereAtual == '*':
+            token = Token(self.caractereAtual, TipoToken.MULTIPLICACAO)
+        elif self.caractereAtual == '/':
+            token = Token(self.caractereAtual, TipoToken.DIVISAO)
+        elif self.caractereAtual.isdigit():
+            posicaoInicial = self.posicaoAtual
+            while self.peek()!=None and self.peek().isdigit():
+                self.proximoCaractere()
+            lexema = self.source[ posicaoInicial : self.posicaoAtual+1 ]
+            token = Token(lexema, TipoToken.NUMERO)
+        elif self.caractereAtual.isalpha():
+            posicaoInicial = self.posicaoAtual
+            while self.peek()!=None and self.peek().isalnum():
+                self.proximoCaractere()
+            lexema = self.source[ posicaoInicial : self.posicaoAtual+1 ]
+            tipo = Token.checaPalavraReservada(lexema)
+            if tipo == None:
+                token = Token(lexema, TipoToken.IDENTIFICADOR)
+            else: 
+                token = Token(lexema, tipo)
+        else:
+            self.abort("Caractere não reconhecido: "+self.caractereAtual)
+        self.proximoCaractere()
+        return token
+
+class Token:
+    def __init__(self, lexema, tipo) -> None:
+        self.lexema = lexema
+        self.tipo = tipo
+    @staticmethod
+    def checaPalavraReservada(lexema):
+        for tipo in TipoToken:
+            if tipo.name == lexema and tipo.value > 100 and tipo.value < 200:
+                return tipo
+        return None
+
+class TipoToken(enum.Enum):
+    EOF = -1
+    NUMERO = 1
+    IDENTIFICADOR = 2
+    SOMA = 202
+    SUBTRACAO = 203
+    MULTIPLICACAO = 204
+    DIVISAO = 205
---
+++ 2024-05-21/simples/parse.py
@@ -0,0 +1,41 @@
+import sys
+from lexer import *
+
+class Parser: 
+    def __init__(self, lexer):
+        self.lexer = lexer
+        self.tokenAtual = None
+        self.proximoToken = None
+        self.nextToken()
+        self.nextToken()
+
+    #Retorna true se o Token **atual** casa com tipo de Token esperado
+    def checkToken(self, tipo):
+        return tipo == self.tokenAtual.tipo
+
+    #Retorna true se o próximo Token **(peek)** casa com tipo de Token esperado
+    def checkPeek(self, tipo):
+        return tipo == self.proximoToken.tipo
+
+    #Tenta fazer o casamento do Token atual. Se conseguir, avança para o próximo Token. Do contrário, gera mensagem de erro.
+    def match(self, tipo):
+        if not self.checkToken(tipo):
+            self.abort("Esperava por token do tipo " + tipo.name + ", mas apareceu " + self.tokenAtual.tipo.name)
+        else:
+            self.nextToken()
+
+    # Avançando com os ponteiros dos tokens (atual e peek)
+    def nextToken(self):
+        self.tokenAtual = self.proximoToken
+        self.proximoToken = self.lexer.getToken()
+
+    def abort(self, msg):
+        sys.exit("Erro sintático: "+msg)
+
+    def parse(self):
+        pass
+
+    # S → aABe
+    # A → Abc
+    # A → b
+    # B → d
